146616af0136c60f3d7b20d43365d9685f8ed773
Thiago <thiagovieiramachado123@hotmail.com>
2020-10-28 18:46:45 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from 'react'
+import Tweet from './Tweet'
 
-function App() {
+
+function App(){
+
+  const [ users , setUsers ] = useState([
+    { name: 'Thiago'   , message: 'Olá' },
+    { name: 'Lucas'    , message: 'Olá2' },
+    { name: 'Leopoldo' , message: 'Olá3' }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
